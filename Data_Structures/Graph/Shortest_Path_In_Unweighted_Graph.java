de3a72f40249dd851b0fc1a1078c4a89581a7f47
aaryam-dev <73819294+aaryam-dev@users.noreply.github.com>
2023-04-09 21:27:34 +0530

    Create Shortest_Path_In_Unweighted_Graph.java

---
+++ Data_Structures/Graph/Shortest_Path_In_Unweighted_Graph.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    
+    static List<Integer> djikstra(int[][] graph, int src) {
+        int V = 4;
+        List<Integer> dist = new ArrayList<>(V);
+        for (int i = 0; i < V; i++) {
+            dist.add(Integer.MAX_VALUE);
+        }
+        dist.set(src, 0);
+        
+        boolean[] fin = new boolean[V];
+        for (int count = 0; count < V - 1; count++) {
+            int u = -1;
+            for (int i = 0; i < V; i++) {
+                if (!fin[i] && (u == -1 || dist.get(i) < dist.get(u))) {
+                    u = i;
+                }
+            }
+            fin[u] = true;
+            
+            for (int v = 0; v < V; v++) {
+                if (graph[u][v] != 0 && !fin[v]) {
+                    dist.set(v, Math.min(dist.get(v), dist.get(u) + graph[u][v]));
+                }
+            }
+        }
+        return dist;
+    }
+    
+    public static void main(String[] args) {
+        int V = 4;
+        int[][] graph = {
+            {0, 50, 100, 0},
+            {50, 0, 30, 200},
+            {100, 30, 0, 20},
+            {0, 200, 20, 0}
+        };
+        
+        for (int x : djikstra(graph, 0)) {
+            System.out.print(x + " ");
+        }
+    }
+}
