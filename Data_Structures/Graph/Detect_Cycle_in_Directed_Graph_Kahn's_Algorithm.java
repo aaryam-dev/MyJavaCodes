0f9a46f7b24ccb8dabbb01612acbc3013c627698
aaryam-dev <73819294+aaryam-dev@users.noreply.github.com>
2023-04-09 21:23:54 +0530

    Create Detect_Cycle_in_Directed_Graph_Kahn's_Algorithm.java

---
+++ Data_Structures/Graph/Detect_Cycle_in_Directed_Graph_Kahn's_Algorithm.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+
+    static void addEdge(ArrayList<Integer>[] adj, int u, int v) {
+        adj[u].add(v);
+    }
+
+    static void DFSrec(ArrayList<Integer>[] adj, int s, boolean[] visited) {
+        visited[s] = true;
+        System.out.print(s + " ");
+        for (int u : adj[s]) {
+            if (!visited[u]) {
+                DFSrec(adj, u, visited);
+            }
+        }
+    }
+
+    static void DFS(ArrayList<Integer>[] adj, int V, int s) {
+        boolean[] visited = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            visited[i] = false;
+        }
+        DFSrec(adj, s, visited);
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<Integer>[] adj = new ArrayList[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        addEdge(adj, 0, 1);
+        addEdge(adj, 0, 2);
+        addEdge(adj, 1, 2);
+        addEdge(adj, 1, 3);
+
+        DFS(adj, V, 0);
+    }
+}
